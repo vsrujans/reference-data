f3ce5925be7ebeb9c7d5d6581ccf287ce0d06b5f
vsrujans <vsrujans@gmail.com>
2020-09-16 22:02:25 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import React from "react";
 var useState = React.useState;
-function GetstateData(lang) {
+function GetstateData(includeAdditionalStates) {
   var countryData = [];
  var labelMap = {};
   var valueMap = {};
-  if (lang === "es") {
-    stateData = require('./local/es/stateList-es.json')
+  if (includeAdditionalStates === "true") {
+    console.log("inside includeAdditionalStates")
+    additionalStateData = require('./local/en/stateList-en-additionalState.json')
+    stateData  =  require('./local/en/stateList-en.json')
+    stateData = stateData.concat(additionalStateData)
+    console.log("state Data: "+stateData)
     stateData.forEach(states => {
       labelMap[states.label.toLowerCase()] = states.value
       valueMap[states.value.toLowerCase()] = states.label
@@ -20,61 +24,54 @@ function GetstateData(lang) {
   }
   return stateData
 }
-function GetCountriesData(lang) {
+function GetCountriesData() {
   var countryData = [];
   var labelMap = {};
   var valueMap = {};
-  if (lang === "es") {
-    countryData = require('./local/es/countryList-es.json')
-    countryData.forEach(country => {
-      labelMap[country.label.toLowerCase()] = country.value
-      valueMap[country.value.toLowerCase()] = country.label
-    })
-  }
-  else {
+ 
     countryData = require('./local/en/countryList-en.json')
     countryData.forEach(country => {
       labelMap[country.label.toLowerCase()] = country.value
       valueMap[country.value.toLowerCase()] = country.label
     })
-  }
+ 
   return countryData
 }
 
-function GetCountyData(lang) {
+function GetCountyData() {
   var countyData = [];
   var labelMap = {};
   var valueMap = {};
-  if (lang === "es") {
-    countyData = require('./local/es/countyList-es.json')
+   countyData = require('./local/en/countyList-en.json')
     countyData.forEach(county => {
       labelMap[county.label.toLowerCase()] = county.value
       valueMap[county.value.toLowerCase()] = county.label
     })
-  }
-  else {
-    countyData = require('./local/en/countyList-en.json')
-    countyData.forEach(county => {
-      labelMap[county.label.toLowerCase()] = county.value
-      valueMap[county.value.toLowerCase()] = county.label
-    })
-  }
+
   return countyData
 }
 
-function GetRelationshipData(lang) {
+function GetRelationshipData(lang,type) {
   var RelationshipData = [];
   var labelMap = {};
   var valueMap = {};
   if (lang === "es") {
-    RelationshipData = require('./local/es/relationshipList-es.json')
+    if(type ==="Birth"){
+    RelationshipData = require('./local/es/relationshipList-Birth-es.json')
+    }else{
+      RelationshipData = require('./local/es/relationshipList-Death-es.json')
+    }
     RelationshipData.forEach(relationship => {
       labelMap[relationship.label.toLowerCase()] = relationship.value
       valueMap[relationship.value.toLowerCase()] = relationship.label
     })
   }
-  else {
-    RelationshipData = require('./local/en/relationshipList-en.json')
+  else { 
+    if(type ==="Birth"){
+    RelationshipData = require('./local/en/relationshipList-Birth-en.json')
+    }else{
+      RelationshipData = require('./local/en/relationshipList-Death-en.json')
+    }
     RelationshipData.forEach(relationship => {
       labelMap[relationship.label.toLowerCase()] = relationship.value
       valueMap[relationship.value.toLowerCase()] = relationship.label
